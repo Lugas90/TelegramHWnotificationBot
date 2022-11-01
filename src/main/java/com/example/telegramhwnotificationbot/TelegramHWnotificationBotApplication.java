ea9d378ac8905f5109b8e517b3479f1853a79107
Lugas90 <Lugas90@mail.ru>
2022-11-01 21:40:55 +0300

    1 комит телеграм бота

---
+++ src/main/java/com/example/telegramhwnotificationbot/TelegramHWnotificationBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.telegramhwnotificationbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramHWnotificationBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramHWnotificationBotApplication.class, args);
+    }
+
+}
